f90c150f8e39caa002280c3bf02f82c03ec4a13e
Jesse Stewart <jesse.stewart@nlsnow.com>
2023-12-11 22:51:55 -0600

    Added table

--- components/BlogDetail.tsx
+++ components/BlogDetail.tsx
@@ -21,43 +21,59 @@ const BlogDetail = ({ post }: any) => {
                             <div className="mr-1 z-10 rounded-full bg-indigo-600 px-3 py-1.5 font-medium text-white">
                                 {post.tags[0]}
                             </div>
-                            <div className="mr-1 z-10 rounded-full bg-indigo-600 px-3 py-1.5 font-medium text-white">
+                            <div className={(post.tags[1] != undefined ? "mr-1 z-10 rounded-full bg-indigo-600 px-3 py-1.5 font-medium text-white": 'hidden')}>
                                 {post.tags[1]}
                             </div>
                         </div>
                         <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{post.title}</h1>
-                        {post.blogContent.json.content?.map((content: any, index: number) => {
-                            if(content.type === 'paragraph') {
-                                return <p key={index} className='text-black'>{content.content[0].text}</p>
-                            }
-                        })}
-                        <p className="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget. Eleifend egestas fringilla sapien.</p>
+                        
                         <div className="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
-                        <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
-                        <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                            <li className="flex gap-x-3">
-                            <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clipRule="evenodd" />
-                            </svg>
-                            <span><strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
-                            </li>
-                            <li className="flex gap-x-3">
-                            <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path fillRule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clipRule="evenodd" />
-                            </svg>
-                            <span><strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
-                            </li>
-                            <li className="flex gap-x-3">
-                            <svg className="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
-                                <path fillRule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clipRule="evenodd" />
-                            </svg>
-                            <span><strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
-                            </li>
-                        </ul>
-                        <p className="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-                        <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-                        <p className="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
+                            {post.blogContent.json.content?.map((content: any, index: number) => {
+                                if(content.type === 'paragraph' && content.content != undefined) {
+
+                                    return <p key={index} className="mt-8">{content.content[0].text}</p>
+                                }
+                                if(content.type === 'heading') {
+                                    return <h2 key={index} className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
+                                        {content.content[0].text}
+                                    </h2>
+                                }
+                                if(content.type === 'table') {
+                                    return (
+                                    <div className="px-4 sm:px-6 lg:px-8">
+                                        <div className="mt-8 flow-root">
+                                            <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                                                <div className="inline-block min-w-full py-2 align-middle">
+                                                <table className="min-w-full divide-y divide-gray-300">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Beach Name</th>
+                                                            <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Information</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody className="divide-y divide-gray-200 bg-white">
+                                                        <tr>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">Teahupo’or</td>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">This legendary surf spot is known for its powerful waves and is considered one of the best places in the world to catch a perfect wave. Teahupo’o is considered a “must-do” for experienced surfers, but be sure to bring your A-game because the waves here can be quite intimidating.</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">Papara</td>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">Located near the town of Papeete, Papara is a great place for experienced surfers to catch some waves and enjoy the scenery. The waves here are known to be quite powerful and consistent, making it a great spot for advanced surfing.</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">PK-18</td>
+                                                            <td className="px-3 py-4 text-sm text-gray-500">Located near the town of Papeete, PK-18 offers some of the best</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  )
+                                }
+                            })}
+
                         </div>
                     </div>
                 </div>
